334dcf0a9ebb21f42c87aeffe196f7e72f51effc
omuphade77 <omuphade77@gmail.com>
2026-03-27 11:27:04 +0530

    issue api done

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,6 +3,12 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
+// Import routes
+const issueRoutes = require("./routes/issue.routes");
+// Use routes
+app.use("/api/issues", issueRoutes);
 
 
 module.exports = app;
